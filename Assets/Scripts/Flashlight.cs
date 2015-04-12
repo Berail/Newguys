a3b4c01359e2e028792066eb43fc8e7366220f57
Luke Nizik <elejdor@gmail.com>
2015-04-12 18:17:44 +0200

    [*] graphical adjustments [+] flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour
+{
+
+    private Light light;
+	// Use this for initialization
+	void Start ()
+	{
+	    light = this.GetComponent<Light>();
+	    light.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.F))
+	    {
+	        light.enabled = !light.enabled;
+	    }
+	}
+}
