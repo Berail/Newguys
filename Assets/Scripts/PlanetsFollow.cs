77a8e4f4572776402730ef9c8cbfb38452a275c8
Luke Nizik <elejdor@gmail.com>
2015-04-13 12:12:40 +0200

    [+] fake sun reflections [*] chenged orbiting method [*] fixed (mostly) shadows flickering

---
+++ Assets/Scripts/PlanetsFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetsFollow : MonoBehaviour
+{
+    private Vector3 offset;
+    [SerializeField] private Transform target;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    offset = this.transform.position - target.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        this.transform.position = target.localPosition + offset;
+	}
+}
